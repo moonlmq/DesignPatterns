eb16dbf8df6e25833bb102ba9cda071fe0737bd7
moonlmq <545427522@qq.com>
2016-08-31 14:10:34 +0800

    eagersingleton

--- Singleton/src/pers/moon/designpattern/singleton/EagerSingleton.java
+++ Singleton/src/pers/moon/designpattern/singleton/EagerSingleton.java
@@ -1,12 +1,12 @@
 package pers.moon.designpattern.singleton;
-/饿汉单例模式
-    //在类加载时就完成了初始化，所以类加载较慢，但获取对象的速度快
+///楗挎眽鍗曚緥妯″紡
+    //鍦ㄧ被鍔犺浇鏃跺氨瀹屾垚浜嗗垵濮嬪寲锛屾墍浠ョ被鍔犺浇杈冩參锛屼絾鑾峰彇瀵硅薄鐨勯�熷害蹇�
 public class EagerSingleton {
 	private static EagerSingleton instance = new EagerSingleton();
 	private EagerSingleton(){
 		System.out.println("come into the Eager construct");
 	}
-	public static LazySingleton getInstance(){
+	public static EagerSingleton getInstance(){
 		return instance;
 	}
 }
